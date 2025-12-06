2a1ebc86cf4104ee51cb57a737b5727ca4df0251
Coder-Sujal <sujalsmit20@gmail.com>
2025-12-07 02:56:30 +0530

    TRPC setup is done

--- flowbuilder/src/app/page.tsx
+++ flowbuilder/src/app/page.tsx
@@ -1,10 +1,19 @@
-import prisma from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import Client from "./Cient";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-const Page = async () => {
-  const user = await prisma.user.findMany();
+
+const Page = async() => {
+
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-        {JSON.stringify(user)}
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <Client />
+        </HydrationBoundary>
     </div>
   );
 };
