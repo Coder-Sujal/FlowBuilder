ad25a0fcb309f19716bba10a0c290cabf77aafc6
Coder-Sujal <sujalsmit20@gmail.com>
2025-12-07 23:54:15 +0530

    Authentication Implemented

---
+++ flowbuilder/src/app/Logout.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
+const Logout = () => {
+  return <Button onClick={() => authClient.signOut()}>Logout</Button>;
+};
+
+export default Logout;
