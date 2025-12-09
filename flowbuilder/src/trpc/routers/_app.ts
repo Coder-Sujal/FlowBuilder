c3693ed6c90af75b2831090fc53f8b278d1d1fbe
Coder-Sujal <sujalsmit20@gmail.com>
2025-12-09 12:42:55 +0530

    Background Job is done

--- flowbuilder/src/trpc/routers/_app.ts
+++ flowbuilder/src/trpc/routers/_app.ts
@@ -1,15 +1,21 @@
-// import { z } from 'zod';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
-export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    console.log(ctx.auth.user.id);
 
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+export const appRouter = createTRPCRouter({
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    console.log(ctx.auth.user.id);
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "sujal20@gmai.com",
       },
     });
+
+    return { success: true, message: "Job queued" };
   }),
 });
 // export type definition of API
