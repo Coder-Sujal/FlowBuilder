2e163506d331ff98b1d0a1247db81257f64c8034
Coder-Sujal <sujalsmit20@gmail.com>
2025-12-10 00:32:13 +0530

    AI Providers added (gemini,, openai, anthropic)

--- flowbuilder/src/app/page.tsx
+++ flowbuilder/src/app/page.tsx
@@ -11,7 +11,7 @@ const Page = () => {
 
   const trpc = useTRPC();
   // const queryClient = useQueryClient();
-  const { data: workflow } = useQuery(trpc.getWorkflows.queryOptions());
+  // const { data: workflow } = useQuery(trpc.getWorkflows.queryOptions());
   const create = useMutation(
     trpc.createWorkflow.mutationOptions({
       onSuccess: () => {
@@ -20,10 +20,17 @@ const Page = () => {
     })
   );
 
+  const createText = useMutation(trpc.testAi.mutationOptions({
+    onSuccess: () => {
+      toast.success("Ai job queued")
+    }
+  }))
+
   return (
     <div className="min-h-screen min-w-screen flex flex-col items-center justify-center gap-6">
       <span>Proptected Component</span>
-      <div className="w-72">{JSON.stringify(workflow)}</div>
+      <div className="w-72">{JSON.stringify(createText.data)}</div>
+      <Button disabled={createText.isPending} onClick={() => createText.mutate()}>Test Ai</Button>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         Create workflow
       </Button>
