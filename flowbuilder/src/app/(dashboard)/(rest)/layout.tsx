6ed478fba7a9b268853fe32440fc97448b515d6e
Coder-Sujal <sujalsmit20@gmail.com>
2025-12-20 03:42:09 +0530

    workflow page is created

--- flowbuilder/src/app/(dashboard)/(rest)/layout.tsx
+++ flowbuilder/src/app/(dashboard)/(rest)/layout.tsx
@@ -2,10 +2,10 @@ import { AppHeader } from "@/components/app-header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex-1 items-center justify-between">
+    <>
       <AppHeader />
-      {children}
-    </main>
+      <main className="flex-1">{children}</main>
+    </>
   );
 };
 
