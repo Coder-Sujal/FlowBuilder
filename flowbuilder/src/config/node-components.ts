a916bcf02bad6795b5f0fe3268dc2809bc9399ff
Coder-Sujal <sujalsmit20@gmail.com>
2025-12-21 23:13:21 +0530

    Different types of nodes added (ex manual trigger node, http request node)

--- flowbuilder/src/config/node-components.ts
+++ flowbuilder/src/config/node-components.ts
@@ -1,9 +1,14 @@
-import { InitialNode } from "@/components/inital-node";
-import { NodeType } from "@/generated/prisma/enums";
 import type { NodeTypes } from "@xyflow/react";
+import { NodeType } from "@/generated/prisma/enums";
+
+import { InitialNode } from "@/components/inital-node";
+import { HttpRequestNode } from "@/features/executions/components/http-request/node";
+import { ManualTriggerNode } from "@/features/triggers/components/manual-trigger/node";
 
 export const nodeComponents = {
   [NodeType.INITIAL]: InitialNode,
+  [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
+  [NodeType.HTTP_REQUEST]: HttpRequestNode,
 } as const satisfies NodeTypes;
 
 export type RegisteredNodeType = keyof typeof nodeComponents;
